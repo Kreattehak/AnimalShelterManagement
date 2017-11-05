ca0ca37a13a47dd8e34182e4896cf79ce05fab8b
Kamil <nerokamil@gmail.com>
2017-11-05 16:49:40 +0100

    PersonController method declarations

--- src/main/java/com/company/AnimalShelterManagement/controller/PersonController.java
+++ src/main/java/com/company/AnimalShelterManagement/controller/PersonController.java
@@ -1,20 +1,38 @@
 package com.company.AnimalShelterManagement.controller;
 
 import com.company.AnimalShelterManagement.model.Person;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PersonController {
 
-    @GetMapping("${rest.getAllPeople}")
-    public Person returnPerson() {
+    @GetMapping("${rest.getPeople}")
+    public Person returnPeople() {
         return new Person();
     }
 
     @GetMapping("${rest.getPerson}")
-    public Person returnPerson(@RequestParam Long personId) {
+    public Person returnPerson(@PathVariable Long personId) {
+        return new Person();
+    }
+
+    @PostMapping("${rest.postPerson")
+    public Person savePerson() {
+        return new Person();
+    }
+
+    @PutMapping("${rest.putPerson")
+    public Person updatePerson(@PathVariable Long personId) {
+        return new Person();
+    }
+
+    @DeleteMapping("${rest.deletePerson")
+    public Person deletePerson(@PathVariable Long personId) {
         return new Person();
     }
 }
