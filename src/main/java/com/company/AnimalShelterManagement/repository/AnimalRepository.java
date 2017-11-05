a7c6a1f2f86a96b858ddd3095daf342ac9559b0a
Kamil <nerokamil@gmail.com>
2017-11-05 18:19:24 +0100

    AnimalRepository fix

--- src/main/java/com/company/AnimalShelterManagement/repository/AnimalRepository.java
+++ src/main/java/com/company/AnimalShelterManagement/repository/AnimalRepository.java
@@ -1,6 +1,7 @@
 package com.company.AnimalShelterManagement.repository;
 
+import com.company.AnimalShelterManagement.model.Animal;
 import org.springframework.data.repository.CrudRepository;
 
-public interface AnimalRepository<T> extends CrudRepository<T, Long> {
+public interface AnimalRepository<T extends Animal> extends CrudRepository<T, Long> {
 }
