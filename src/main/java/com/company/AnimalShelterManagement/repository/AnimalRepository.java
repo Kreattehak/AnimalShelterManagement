f4987b74d33911b6177373850f7d9c90ebc8ce5b
Kamil <nerokamil@gmail.com>
2017-11-06 14:47:39 +0100

    Delete AnimalRepository cause of @MappedSuperclass we can't get common data for any animal in database

--- src/main/java/com/company/AnimalShelterManagement/repository/AnimalRepository.java
+++
@@ -1,8 +0,0 @@
-package com.company.AnimalShelterManagement.repository;
-
-import com.company.AnimalShelterManagement.model.Animal;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AnimalRepository extends CrudRepository<Animal, Long> {
-
-}
