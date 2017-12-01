2c9b7b21accfcb4a75f23c997e3dad37ffd2775c
Kreattehak <nerokamil@gmail.com>
2017-12-01 19:45:56 +0100

    [B] AnimalShelterException now keeps order of params in exception.
    Method toMap() now use LinkedHashMap to remember order of parameters
    passed to the constructor of new exception.

--- backend/src/main/java/com/company/AnimalShelterManagement/utils/AnimalShelterException.java
+++ backend/src/main/java/com/company/AnimalShelterManagement/utils/AnimalShelterException.java
@@ -1,6 +1,6 @@
 package com.company.AnimalShelterManagement.utils;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.stream.IntStream;
 
@@ -20,7 +20,7 @@ class AnimalShelterException extends RuntimeException {
 
         return IntStream.range(0, entries.length / 2)
                 .map(i -> i * 2)
-                .collect(HashMap::new,
+                .collect(LinkedHashMap::new,
                         (m, i) -> m.put(entries[i], entries[i + 1]),
                         Map::putAll);
     }
