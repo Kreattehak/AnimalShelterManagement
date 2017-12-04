d0676d1073173ee494f69f8dcb4713fb0477c596
Kreattehak <nerokamil@gmail.com>
2017-12-04 17:27:25 +0100

    [B] Update data.sql and data-test.sql with date_of_registration column

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -6,9 +6,9 @@ INSERT INTO `person_address` VALUES (1, 1), (1, 2);
 
 INSERT INTO `person_main_address` VALUES (1, 1);
 
-INSERT INTO `animal` VALUES (1, '00150001', 'ADOPTED', 'Good boy', CURDATE(), 'Sparky', 'DOG'),
-  (2, '00150002', 'BEFORE_VACCINATION', 'Sleeps all day', CURDATE(), 'Puffy', 'CAT'),
-  (3, '00150003', 'AVAILABLE', 'Such a boy', CURDATE(), 'Fire', 'DOG');
+INSERT INTO `animal` VALUES (1, '00150001', 'ADOPTED', 'Good boy', CURDATE(), CURDATE(), 'Sparky', 'DOG'),
+  (2, '00150002', 'BEFORE_VACCINATION', 'Sleeps all day', CURDATE(), CURDATE(), 'Puffy', 'CAT'),
+  (3, '00150003', 'AVAILABLE', 'Such a boy', CURDATE(), CURDATE(), 'Fire', 'DOG');
 
 INSERT INTO `dog` VALUES ('GERMAN_SHEPERD', 1), ('CROSSBREAD', 3);
 
