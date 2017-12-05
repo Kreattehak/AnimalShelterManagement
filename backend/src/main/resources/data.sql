8d28c61e1da1b684b7c690d850aba76f569a97f0
Kreattehak <nerokamil@gmail.com>
2017-12-05 18:18:50 +0100

    [B] Update SQL to have proper different date of birth

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -6,9 +6,9 @@ INSERT INTO `person_address` VALUES (1, 1), (1, 2);
 
 INSERT INTO `person_main_address` VALUES (1, 1);
 
-INSERT INTO `animal` VALUES (1, '00150001', 'ADOPTED', 'Good boy', CURDATE(), CURDATE(), 'Sparky', 'DOG'),
-  (2, '00150002', 'BEFORE_VACCINATION', 'Sleeps all day', CURDATE(), CURDATE(), 'Puffy', 'CAT'),
-  (3, '00150003', 'AVAILABLE', 'Such a boy', CURDATE(), CURDATE(), 'Fire', 'DOG');
+INSERT INTO `animal` VALUES (1, '00150001', 'ADOPTED', 'Good boy', '1999-11-06', CURDATE(), 'Sparky', 'DOG'),
+  (2, '00150002', 'BEFORE_VACCINATION', 'Sleeps all day', '2010-11-12', CURDATE(), 'Puffy', 'CAT'),
+  (3, '00150003', 'AVAILABLE', 'Such a boy', '2005-04-30', CURDATE(), 'Fire', 'DOG');
 
 INSERT INTO `dog` VALUES ('GERMAN_SHEPERD', 1), ('CROSSBREAD', 3);
 
