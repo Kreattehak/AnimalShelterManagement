2120a336cc942edf8955d42dabf0002033e64c2c
Kreattehak <nerokamil@gmail.com>
2017-12-23 18:28:43 +0100

    [F]Add Page class to get Pageable responses from spring backend

---
+++ frontend/src/app/shared/page.ts
@@ -0,0 +1,11 @@
+export class Page<T> {
+  content: T;
+  last: boolean;
+  totalPages: number;
+  totalElements: number;
+  size: number;
+  number: number;
+  sort: any;
+  numberOfElements: number;
+  first: boolean;
+}
